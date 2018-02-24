662a5935cd6920d4fd5fce0498d314e3f042583d
oleg <olegmarkovbox@gmail.com>
2018-02-24 09:56:11 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function f1 () {
+    alert("Hello, it is my first JS code")
+}
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
